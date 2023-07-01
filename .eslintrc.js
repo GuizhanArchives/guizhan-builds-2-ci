044a36b465449c551d4cc8d601bc1a49bf5cb8e8
ybw0014 <ybw0014@outlook.com>
2023-07-01 13:50:35 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'script'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+  }
+}
